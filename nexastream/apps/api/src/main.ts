d82e93ed813e666dc84bbb9b8f20bd8bc2d7cfcf
grupogtvls-crypto <grupo.gtvls@gmail.com>
2026-03-24 00:28:50 -0300

    Update main.ts

--- nexastream/apps/api/src/main.ts
+++ nexastream/apps/api/src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
 
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
